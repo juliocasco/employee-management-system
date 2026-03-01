aa9e8e9c6b246f75d97c5874645b9f0387ef88d1
Julio Cascoe <Juliocasco@usf.edu>
2026-03-01 18:45:14 -0500

    Created Initial Files Base Class Employees

---
+++ cc_6.js
@@ -0,0 +1,12 @@
+// Base Class: Employee
+class Employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+    describe() {
+        return `${this.name} works in the ${this.department} department.`;
+    }
+}
+
+// Class: Project
